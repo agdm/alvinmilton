c0a7bbad4ae99b3d6957789ba1aebf14b08b1ff7
Alvin Milton <alvin.milton@gmail.com>
2012-01-21 14:46:39 -0500

    troubleshooting - installing gems

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source :rubygems
-gem 'sinatra', :git => 'git://github.com/sinatra/sinatra.git'
+gem 'sinatra'
+gem 'haml'
+gem 'sass'
--- am.rb
+++ am.rb
@@ -2,11 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
+
+set :haml, :format => :html5
 
 not_found do
 	'What you are looking for can not be found via shenanigans'
 end
 
 get '/' do
-	haml :index, :format => :html5
+	haml :index
 end
